d2aec55756f2aa4babc6f709ce73bb3ae3ee2f74
melanie <114537207+todayisthorsday@users.noreply.github.com>
2025-01-23 21:11:55 -0330

    Edited code so that program would run successfully. Tested all flags. Created package.json file.

--- CLIindex.js
+++ CLIindex.js
@@ -3,239 +3,62 @@
 const process = require("node:process");
 const userArgs = process.argv.slice(2);
 
-// indexes of characters
-
 const characters1 = [
-  "a",
-  "b",
-  "c",
-  "d",
-  "e",
-  "f",
-  "g",
-  "h",
-  "i",
-  "j",
-  "k",
-  "l",
-  "m",
-  "n",
-  "o",
-  "p",
-  "q",
-  "r",
-  "s",
-  "t",
-  "u",
-  "v",
-  "w",
-  "x",
-  "y",
-  "z",
+  "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m",
+  "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"
 ];
 
 const characters2 = [
-  "a",
-  "b",
-  "c",
-  "d",
-  "e",
-  "f",
-  "g",
-  "h",
-  "i",
-  "j",
-  "k",
-  "l",
-  "m",
-  "n",
-  "o",
-  "p",
-  "q",
-  "r",
-  "s",
-  "t",
-  "u",
-  "v",
-  "w",
-  "x",
-  "y",
-  "z",
-  "A",
-  "B",
-  "C",
-  "D",
-  "E",
-  "F",
-  "G",
-  "H",
-  "I",
-  "J",
-  "K",
-  "L",
-  "M",
-  "N",
-  "O",
-  "P",
-  "Q",
-  "R",
-  "S",
-  "T",
-  "U",
-  "V",
-  "W",
-  "X",
-  "Y",
-  "Z",
+  "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m",
+  "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",
+  "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M",
+  "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"
 ];
 
 const characters3 = [
-  "A",
-  "B",
-  "C",
-  "D",
-  "E",
-  "F",
-  "G",
-  "H",
-  "I",
-  "J",
-  "K",
-  "L",
-  "M",
-  "N",
-  "O",
-  "P",
-  "Q",
-  "R",
-  "S",
-  "T",
-  "U",
-  "V",
-  "W",
-  "X",
-  "Y",
-  "Z",
-  "a",
-  "b",
-  "c",
-  "d",
-  "e",
-  "f",
-  "g",
-  "h",
-  "i",
-  "j",
-  "k",
-  "l",
-  "m",
-  "n",
-  "o",
-  "p",
-  "q",
-  "r",
-  "s",
-  "t",
-  "u",
-  "v",
-  "w",
-  "x",
-  "y",
-  "z",
-  "0",
-  "1",
-  "2",
-  "3",
-  "4",
-  "5",
-  "6",
-  "7",
-  "8",
-  "9",
-  "~",
-  "`",
-  "!",
-  "@",
-  "#",
-  "$",
-  "%",
-  "^",
-  "&",
-  "*",
-  "(",
-  ")",
-  "_",
-  "-",
-  "+",
-  "=",
-  "{",
-  "[",
-  "}",
-  "]",
-  ",",
-  "|",
-  ":",
-  ";",
-  "<",
-  ">",
-  ".",
-  "?",
-  "/",
+  "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M",
+  "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",
+  "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m",
+  "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",
+  "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "~", "`", "!",
+  "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "+", "=",
+  "{", "[", "}", "]", ",", "|", ":", ";", "<", ">", ".", "?", "/"
 ];
 
-// generate passwords function
 function generate() {
-  let passwordString = "";
-  for (let i = 0; i < length; i++) {
-    // loops through index according to the .length input
-    if (userArgs.includes("--generate")) {
-      let index = Math.floor(Math.random() * characters1.length); // gets random number from index
-      passwordString += characters1[index]; // changes index numbers to the content strings instead
-      console.log(passwordString);
+    let characters;
+    if (userArgs.includes("--all") || userArgs.includes("-a")) {
+        characters = characters3;
+    } else if (userArgs.includes("--upper") || userArgs.includes("-u")) {
+        characters = characters2;
+    } else {
+        characters = characters1;
     }
-    if (userArgs.includes("--upper")) {
+
+    let passwordString = "";
+    for (let i = 0; i < 15; i++) {
+        const index = Math.floor(Math.random() * characters.length);
+        passwordString += characters[index];
     }
-    let index = Math.floor(Math.random() * characters2.length); // gets random number from index
-    passwordString += characters2[index]; // changes index numbers to the content strings instead
-    console.log(passwordString);
-  }
-  if (userArgs.includes("--all")) {
-    let index = Math.floor(Math.random() * characters3.length); // gets random number from index
-    passwordString += characters3[index]; // changes index numbers to the content strings instead
-    console.log(passwordString);
-  }
+    console.log(`Generated Password: ${passwordString}`);
 }
 
 function printHelpMessage() {
-  console.log("Usage: password-generator [options]");
-  console.log("Options:");
-  console.log("  -h, --help      Print this message");
-  console.log("  -g, --generate  Generate a new password");
-  console.log(
-    "  -u, --upper     Add upercase characters to the generated password"
-  );
-  console.log(
-    "  -a, --all     Add uppercase, number, and symbol characters to the generated password"
-  );
+    console.log()
+    console.log("Password-generator usage options:");
+    console.log("  -h, --help        Print this message");
+    console.log("  -g, --generate    Generate a password");
+    console.log()
+    console.log("To increase password complexity please add one of the following to your command");  
+    console.log("  -u, --upper       Include uppercase characters");
+    console.log("  -a, --all         Include all characters (uppercase, numbers, symbols)");
+    console.log()
 }
 
-if (userArgs.includes("--generate") || userArgs.includes("-g")) {
-  generate();
-} else if (
-  userArgs.includes("--help") ||
-  userArgs.includes("-h") ||
-  userArgs.includes("help")
-) {
+if (userArgs.includes("--help") || userArgs.includes("-h")) {
   printHelpMessage();
+} else if (userArgs.includes("--generate") || userArgs.includes("-g")) {
+  generate();
 } else {
-  console.log(
-    "No valid arguments provided. Use `--help` for usage information."
-  );
-}
-
-function handleArguments(userArguments) {
-  if (userArguments.length === 0) {
-    printHelpMessage();
-    return;
-  }
+  console.log("No valid arguments provided. Use `--help` for usage information.");
 }
